0e3bc9cf5314f85871c0b323c09b9a00b816ab4a
王家龙 <wangjialong@corp.netease.com>
2018-08-06 15:34:53 +0800

    删除无用的目录

--- src/router/index.js
+++ src/router/index.js
@@ -39,295 +39,32 @@ export const constantRouterMap = [
     }]
   },
   {
-    path: '/resource',
+    path: '/content',
     component: Layout,
     redirect: 'noredirect',
-    name: 'resource',
-    meta: {
-      title: '资源位管理',
-      icon: 'example'
-    },
-    children: [
-      {
-        path: '/resource/periodical',
-        name: 'periodical',
-        component: _import('periodical/index'),
-        redirect: '/resource/periodical/periodicals',
-        meta: {
-          title: '先锋内刊管理',
-          icon: 'example'
-        },
-        children: [
-          {
-            path: 'periodicals',
-            name: 'periodicals',
-            component: _import('periodical/PeriodicalList'),
-            meta: {
-              title: '先锋内刊管理',
-              icon: 'example'
-            }
-          },
-          {
-            path: 'periodical-edit/:id',
-            name: 'periodical-edit',
-            component: _import('periodical/PeriodicalEdit'),
-            hidden: true,
-            meta: {
-              title: '先锋内刊编辑',
-              icon: 'example'
-            }
-          }
-        ]
-      },
-      {
-        path: '/resource/activity',
-        name: 'activity',
-        component: _import('activity/index'),
-        redirect: '/resource/activity/Promotions',
-        hidden: true,
-        meta: {
-          title: '活动管理',
-          icon: 'example'
-        },
-        children: [
-          {
-            path: 'promotions',
-            name: 'promotions',
-            component: _import('activity/Promotions'),
-            meta: {
-              title: '活动管理',
-              icon: 'example'
-            }
-          },
-          {
-            path: 'promotions-edit/:id',
-            name: 'promotions-edit',
-            component: _import('activity/PromotionsEdit'),
-            hidden: true,
-            meta: {
-              title: '活动管理配置',
-              icon: 'example'
-            }
-          }
-        ]
-      },
-      {
-        path: '/resource/launch',
-        name: 'launch',
-        component: _import('launch/index'),
-        redirect: '/resource/launch/Startpages',
-        hidden: true,
-        meta: {
-          title: '启动页管理',
-          icon: 'example'
-        },
-        children: [
-          {
-            path: 'startpages',
-            name: 'startpages',
-            component: _import('launch/Startpages'),
-            meta: {
-              title: '启动页管理',
-              icon: 'example'
-            }
-          },
-          {
-            path: 'startpages-edit/:id',
-            name: 'startpages-edit',
-            component: _import('launch/StartpagesEdit'),
-            hidden: true,
-            meta: {
-              title: '启动页管理配置',
-              icon: 'example'
-            }
-          }
-        ]
-      },
-      {
-        path: '/resource/daily',
-        name: 'daily',
-        component: _import('dailySignIn/index'),
-        redirect: '/resource/daily/daily-sign-in',
-        meta: {
-          title: '日签管理',
-          icon: 'example'
-        },
-        children: [
-          {
-            path: 'daily-sign-in',
-            name: 'daily-sign-in',
-            component: _import('dailySignIn/DailySignInList'),
-            meta: {
-              title: '日签管理',
-              icon: 'example'
-            }
-          },
-          {
-            path: 'daily-sign-in-edit/:id',
-            name: 'daily-sign-in-edit',
-            component: _import('dailySignIn/DailySignInEdit'),
-            hidden: true,
-            meta: {
-              title: '日签编辑',
-              icon: 'example'
-            }
-          }
-        ]
-      },
-      {
-        path: '/resource/lottery',
-        name: 'lottery',
-        component: _import('lottery/index'),
-        redirect: '/resource/lottery/lottery-list',
-        meta: {
-          title: '抽奖管理',
-          icon: 'example'
-        },
-        children: [
-          {
-            path: 'lottery-list',
-            name: 'lottery-list',
-            component: _import('lottery/LotteryList'),
-            meta: {
-              title: '抽奖管理',
-              icon: 'example'
-            }
-          },
-          {
-            path: 'lottery-edit/:id',
-            name: 'lottery-edit',
-            component: _import('lottery/LotteryEdit'),
-            hidden: true,
-            meta: {
-              title: '抽奖编辑',
-              icon: 'example'
-            }
-          }
-        ]
-      },
-      {
-        path: 'feeds',
-        name: 'feeds',
-        component: _import('feeds/index'),
-        meta: {
-          title: '推荐流管理',
-          icon: 'example'
-        }
-      }
-    ]
-  },
-  {
-    path: '/users',
-    component: Layout,
-    redirect: 'noredirect',
-    name: 'users',
-    meta: {
-      title: '用户相关',
-      icon: 'example'
-    },
-    children: [
-      {
-        path: '/users/identity',
-        name: 'identity',
-        component: _import('identity/index'),
-        redirect: '/users/identity/identity-type',
-        meta: {
-          title: '身份管理',
-          icon: 'example'
-        },
-        children: [
-          {
-            path: 'identity-type',
-            name: 'identity-type',
-            component: _import('identity/IdentityList'),
-            meta: {
-              title: '身份管理',
-              icon: 'example'
-            }
-          },
-          {
-            path: 'identity-user',
-            name: 'identity-user',
-            component: _import('identity/UserIdentityList'),
-            hidden: true,
-            meta: {
-              title: '身份用户管理',
-              icon: 'example'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  {
-    path: '/shop',
-    component: Layout,
-    redirect: 'noredirect',
-    name: 'shop',
+    name: 'content',
     hidden: true,
     meta: {
-      title: '商城',
+      title: '内容管理',
       icon: 'example'
     },
     children: [
       {
-        path: '/shop/products',
-        name: 'products',
-        component: _import('shop/index'),
-        redirect: '/shop/products/product-list',
+        path: '/content/picpick',
+        name: 'picpick',
+        component: _import('picpick/index'),
+        redirect: '/content/picpick/picpick-list',
         meta: {
-          title: '商品管理',
+          title: '图选管理',
           icon: 'example'
         },
         children: [
           {
-            path: 'product-list',
-            name: 'product-list',
-            component: _import('shop/ProductList'),
+            path: 'picpick-list',
+            name: 'picpick-list',
+            component: _import('picpick/PicpickList'),
             meta: {
-              title: '商品管理',
-              icon: 'example'
-            }
-          },
-          {
-            path: 'product-edit/:id',
-            name: 'product-edit',
-            component: _import('shop/ProductEdit'),
-            hidden: true,
-            meta: {
-              title: '商品详情',
-              icon: 'example'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  {
-    path: '/other',
-    component: Layout,
-    redirect: 'noredirect',
-    name: 'other',
-    meta: {
-      title: '其他',
-      icon: 'example'
-    },
-    children: [
-      {
-        path: '/other/xcx',
-        component: _import('other/index'),
-        redirect: '/other/xcx-order',
-        meta: {
-          title: '菜单解析',
-          icon: 'example'
-        },
-        children: [
-          {
-            path: 'xcx-order',
-            name: 'xcxexp',
-            component: _import('other/XcxOrder'),
-            meta: {
-              title: '菜单解析',
+              title: '图选管理',
               icon: 'example'
             }
           }
